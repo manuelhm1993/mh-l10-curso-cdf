da2719c07742234bc8b8fd1f7b99a723994bf751
Manuel Henriquez <manuelhm1993@gmail.com>
2023-09-16 02:02:36 -0400

    Reescribir las propiedades del modelo Si el modelo no sigue la convención, se puede especificar la tabla que adminsitra a través de la propiedad protected Dicha propiedad recibe un string con el nombre de la tabla que va a corresponder al modelo

--- app/Models/Curso.php
+++ app/Models/Curso.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Curso extends Model
 {
     use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'users';
 }
