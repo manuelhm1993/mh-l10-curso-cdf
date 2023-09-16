9c6d4189c62a38949a75df82099ba25340311264
Manuel Henriquez <manuelhm1993@gmail.com>
2023-09-16 04:51:23 -0400

    Construcción de una vista usando todos los cursos de la BD, MVC completo

--- resources/views/cursos/index.blade.php
+++ resources/views/cursos/index.blade.php
@@ -4,4 +4,17 @@
 
 @section('content')
     <h1>Bienvenido a la página principal de cursos</h1>
+
+    <section>
+        <h2>Listado de cursos</h2>
+
+        @foreach ($cursos as $curso)
+            <ul>
+                <li>Nombre: {{ $curso->name }}</li>
+                <li>Categoría: {{ $curso->categoria }}</li>
+                <li>Descripción: {{ $curso->description }}</li>
+            </ul>
+            <hr>
+        @endforeach
+    </section>
 @endsection
